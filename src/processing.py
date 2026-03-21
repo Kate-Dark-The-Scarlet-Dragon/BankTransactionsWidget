b11a9a5624a32bce2ccb5e07c1a6398b0a259210
Kate_Dark The_Scarlet_Dragon <thescarletdragon@yandex.ru>
2026-03-21 23:07:59 +0300

    Добавлена функция sort_by_date в модуль processing пакета src

--- src/processing.py
+++ src/processing.py
@@ -8,3 +8,15 @@ def filter_by_state(operation_list: list[dict], state: str = "EXECUTED") -> list
     filtered_operation_list = [operation for operation in operation_list if operation["state"] == state]
 
     return filtered_operation_list
+
+
+def sort_by_date(operation_list: list[dict], descending: bool = True) -> list[dict]:
+    """
+    Сортировка операций по дате
+    :param operation_list: Список операций
+    :param descending: Признак - сортировка по убыванию. По умолчанию - True (да)
+    :return: Отсортированный по дате список операций
+    """
+    sorted_operation_list = sorted(operation_list, key=lambda x: x["date"], reverse=descending)
+
+    return sorted_operation_list
