bc4618f587fec95295df66ec2be95cd57de570ad
Kate_Dark The_Scarlet_Dragon <thescarletdragon@yandex.ru>
2026-03-25 22:55:40 +0300

    Изменён модуль masks: - Изменены типы входных параметров функций с числового на строковый, т.к. функция модуля widget номера получает из строк; - Расширена документация к функциям; - Добавлены проверки на входные параметры

--- src/masks.py
+++ src/masks.py
@@ -1,19 +1,35 @@
-def get_mask_card_number(card_number: int) -> str:
-    """Функция, которая форматирует номер карты и выводит маску"""
-    card_number_str = str(card_number)
+def get_mask_card_number(card_number: str) -> str:
+    """
+    Функция, которая форматирует номер карты и выводит маску
+    :param card_number: Номер карты
+    :return: Маска номера карты
+    """
+    if len(card_number) == 0:
+        raise ValueError("Отсутствует номер карты")
 
-    card_parts = [card_number_str[i : i + 4] for i in range(0, len(card_number_str), 4)]
+    if len(card_number) != 16 or card_number.isdecimal():
+        raise ValueError("Номер карты должен состоять из 16 цифр")
+
+    card_parts = [card_number[i : i + 4] for i in range(0, len(card_number), 4)]
 
     masked_card_number = f"{card_parts[0]} {card_parts[1][:2]}** **** {card_parts[-1]}"
 
     return masked_card_number
 
 
-def get_mask_account(account_number: int) -> str:
-    """Функция, которая форматирует номер лицевого счёта и выводит маску"""
-    account_number_str = str(account_number)
+def get_mask_account(account_number: str) -> str:
+    """
+    Функция, которая форматирует номер лицевого счёта и выводит маску
+    :param account_number: Номер лицевого счёта
+    :return: Маска номера лицевого счёта
+    """
+    if len(account_number) == 0:
+        raise ValueError("Отсутствует номер лицевого счёта")
 
-    account_last_numbers = account_number_str[-4:]
+    if len(account_number) != 20 or account_number.isdecimal():
+        raise ValueError("Номер лицевого счёта состоять из 20 цифр")
+
+    account_last_numbers = account_number[-4:]
 
     masked_account_number = f"**{account_last_numbers}"
 
