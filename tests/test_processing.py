e3d2f3908629a1e21dcabd623e5d75f9e8cf5c38
Kate_Dark The_Scarlet_Dragon <thescarletdragon@yandex.ru>
2026-03-30 23:18:01 +0300

    Изменения по замечанию наставника SkyPro: - Удалёна библиотека pytest-lazy-fixture; - Переписаны тесты для модуля processing в связи с предыдущим пунктом; - Изменён README - для тестов выделен отдельный блок

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,30 +1,18 @@
-import pytest
-from pytest_lazy_fixtures import lf
-
 from src import processing
 
 
-@pytest.mark.parametrize(
-    "operations, state, expected",
-    [
-        (lf("operation_list"), "EXECUTED", lf("filter_result_executed")),
-        (lf("operation_list"), "CANCELED", lf("filter_result_canceled")),
-    ],
-)
-def test_filter_by_state(operations, state, expected):
-    assert processing.filter_by_state(operations, state) == expected
+def test_filter_by_state(operation_list, filter_result_executed, filter_result_canceled):
+    assert processing.filter_by_state(operation_list) == filter_result_executed
+    assert processing.filter_by_state(operation_list, "CANCELED") == filter_result_canceled
 
 
 def test_filter_by_state_with_empty_list():
     assert processing.filter_by_state([]) == []
 
 
-@pytest.mark.parametrize(
-    "operations, is_descending, expected",
-    [(lf("operation_list"), False, lf("sort_result_asc")), (lf("operation_list"), True, lf("sort_result_desc"))],
-)
-def test_sort_by_date(operations, is_descending, expected):
-    assert processing.sort_by_date(operations, is_descending) == expected
+def test_sort_by_date(operation_list, sort_result_asc, sort_result_desc):
+    assert processing.sort_by_date(operation_list, False) == sort_result_asc
+    assert processing.sort_by_date(operation_list) == sort_result_desc
 
 
 def test_sort_by_date_with_empty_list():
